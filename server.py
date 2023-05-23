34e9f7c6d0f2675e929173d5672986b118ec8dce
Nathan Touchton <ntouchton@protonmail.com>
2023-05-23 16:44:03 -0500

    modified:   server.py 	modified:   templates/index.html

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from random import randint
+from datetime import date
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,4 +7,5 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     random_number = randint(1, 10)
-    return render_template("index.html", num=random_number)
+    year = date.today().year
+    return render_template("index.html", num=random_number, year=year)
