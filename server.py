a2db94e9bbc5e29d7d8e90f9454d2d2a33937c0d
Nathan Touchton <ntouchton@protonmail.com>
2023-05-23 21:23:27 -0500

    modified:   server.py 	new file:   templates/name-game.html

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from random import randint
 from datetime import date
+from json import loads
+from requests import get
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,3 +11,20 @@ def home():
     random_number = randint(1, 10)
     year = date.today().year
     return render_template("index.html", num=random_number, year=year)
+
+@app.route("/guess/<name>")
+def guess(name):
+    parameters = {
+        "name": name
+    }
+    age_response = get(url="https://api.agify.io", params=parameters, timeout=10)
+    age_response.raise_for_status()
+    age = loads(age_response.text)["age"]
+
+    gender_response = get("https://api.genderize.io", params=parameters, timeout=10)
+    gender_response.raise_for_status()
+    gender = loads(gender_response.text)["gender"]
+    print(gender)
+
+    return render_template("name-game.html", name=name, age=age, gender=gender)
+
