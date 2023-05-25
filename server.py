b850c0ae54a1ce85230c64e4cd21de5382add197
Nathan Touchton <ntouchton@protonmail.com>
2023-05-24 21:17:47 -0500

    new file:   post.py 	modified:   server.py 	new file:   static/css/styles.css 	deleted:    templates/blog.html 	modified:   templates/index.html 	deleted:    templates/name-game.html 	new file:   templates/post.html

--- server.py
+++ server.py
@@ -1,34 +1,13 @@
-from random import randint
-from datetime import date
-from requests import get
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+###Using the API for our blog posts we created on n:Point,
+# render all the blogs' title and subtitles on the home page.###
+
+###Make a "Read" anchor tag at the end of each blog post preview link to a page with the entire blog - title, subtitle and body.
+# The individual blog posts should live at the path: URL/post/blog_id###
+
+@app.route('/')
 def home():
-    random_number = randint(1, 10)
-    year = date.today().year
-    return render_template("index.html", num=random_number, year=year)
-
-@app.route("/guess/<name>")
-def guess(name):
-    parameters = {
-        "name": name
-    }
-    age_response = get(url="https://api.agify.io", params=parameters, timeout=10)
-    age_response.raise_for_status()
-    age = age_response.json()["age"]
-
-    gender_response = get(url="https://api.genderize.io", params=parameters, timeout=10)
-    gender_response.raise_for_status()
-    gender = gender_response.json()["gender"]
-
-    return render_template("name-game.html", name=name, age=age, gender=gender)
-
-@app.route("/blog")
-def blog():
-    blog_response = get(url="https://api.npoint.io/c790b4d5cab58020d391", timeout=10)
-    blog_response.raise_for_status()
-    text = blog_response.json()
-    return render_template("blog.html", text=text)
+    return render_template("index.html")
