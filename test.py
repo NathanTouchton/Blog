e8582912659e2d6090b2d0b1d9a945d1863bd64e
Nathan Touchton <ntouchton@protonmail.com>
2023-05-30 12:22:13 -0500

    modified:   templates/index.html 	deleted:    test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from pprint import pprint
-from requests import get
-
-response = get(url="https://api.npoint.io/c790b4d5cab58020d391", timeout=10)
-response.raise_for_status()
-
-pprint(response.json())
